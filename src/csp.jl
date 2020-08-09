0bf509df83d13bb9266ea6c50fc5b6f3e0954e94
Nils Martel <nils.martel@graphmasters.net>
2020-08-09 12:18:07 +0200

    Implement proper Constraint Type

--- src/csp.jl
+++ src/csp.jl
@@ -1,13 +1,23 @@
 
 using LinearAlgebra.cross
 
-struct CSP{V, T}
-    variables :: Array{String,1}
-    domains :: Array{T, 1}
-    constraints
+struct Constraint{X}
+    # Variables checked by this constraint
+    uses :: Array{X,1}
+    # Function to check if the constraint is satisfied.
+    # Arguments need to be passed in the order they are present in `uses`.
+    # Fn(...D) -> Boolean
+    check_function
+end
+
+struct CSP{X, D}
+    variables :: Array{X,1}
+    domains :: Array{D, 1}
+    constraints :: Array{Constraint{X},1}
 end
 
 function sudoko_csp() :: CSP{(UInt,UInt),UInt}
     variables = cross(0:9, 0:9) .|> map do (x, y)
-
-    
+    domains = 0:9
+    constraints
+end
