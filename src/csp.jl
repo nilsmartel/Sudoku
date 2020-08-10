c8e6122d460357c107106b5086b7edcdaa26b1c0
Nils Martel <nils.martel@graphmasters.net>
2020-08-10 15:57:08 +0200

    Implement proper constraints for sudoku

--- src/csp.jl
+++ src/csp.jl
@@ -1,23 +1,62 @@
 
-using LinearAlgebra.cross
-
 struct Constraint{X}
     # Variables checked by this constraint
     uses :: Array{X,1}
     # Function to check if the constraint is satisfied.
-    # Arguments need to be passed in the order they are present in `uses`.
-    # Fn(...D) -> Boolean
-    check_function
+    # Arguments need to be of domain type and passed in the order they are present in `uses`.
+    checkfunction
 end
 
-struct CSP{X, D}
+struct Csp{X, D}
     variables :: Array{X,1}
     domains :: Array{D, 1}
     constraints :: Array{Constraint{X},1}
 end
 
-function sudoko_csp() :: CSP{(UInt,UInt),UInt}
-    variables = cross(0:9, 0:9) .|> map do (x, y)
-    domains = 0:9
-    constraints
+function cross(a, b)
+    v = []
+    for first in a
+        for second in b
+            push!(v, (first, second))
+        end
+    end
+    return v
 end
+
+function stepnum(number :: UInt, step :: UUInt) :: UInt {
+    floor(number / step) * step
+}
+
+# returns a list of all fields a given position on a sudoku grid
+# needs to differ from
+function related_fields(field :: (UInt, UInt)) :: Set{(UInt, UInt)}
+    row = stepnum(field[2], 3)
+    col = stepnum(field[1], 3)
+
+    squares = cross(row:(row+3), col:(col+3))
+    vertical = map(0:9) do y (field[1], y) end
+    horizontal = map(0:9) do x (x, field[2]) end
+    
+    s = Set(squares..., vertical..., horizontal...)
+
+    # fields musn't be unequal to iteself, so we remove it from the set
+    delete!(s, field)
+
+    s
+end
+    
+
+function sudoko_csp() :: Csp{(UInt,UInt),UInt}
+    variables = cross(0:9, 0:9)::Array{(UInt,UInt), 1}
+    domains = 0:9
+    constraints = begin
+        variables |> map do var
+            rel = related_fields(var)
+            map(rel) do other Constraint([var, other], !=) end
+        end
+    end
+
+    Csp(variables, domains, constraints)
+end
+
+sample = sudoko_csp()
