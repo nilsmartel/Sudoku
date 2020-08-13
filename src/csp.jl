952bfa18b71eb5c3bca256193fd85449f5bf67e3
Nils Martel <nils.martel@graphmasters.net>
2020-08-13 16:32:30 +0200

    Implement AC3 and solution checking

--- src/csp.jl
+++ src/csp.jl
@@ -1,4 +1,3 @@
-
 struct Constraint{X}
     # Variables checked by this constraint
     uses :: Array{X,1}
@@ -79,4 +78,88 @@ function sudoko_csp() :: Csp{Tuple{Int,Int},Int}
     Csp(variables, domains, vcat(constraints...))
 end
 
-sample = sudoko_csp()
+function issolution(csp, assignment :: Dict{Tuple{Int, Int}, Set{Int}})
+    d = Dict{Tuple{Int, Int}, Int}
+    for k in assignment.keys
+        v = assignment[k]
+        if length(v) != 1
+            return nothing
+        end
+
+        d[k] = v.dict.keys[1]
+    end
+
+    for constraint::Constraint in csp.constraints
+        args = constraint.uses .|> var -> d[var]
+        if ! csp.checkfunction(args...)
+            return nothing
+        end
+    end
+
+    d
+end
+
+# enforces arc consistency using the ac3 algorithm
+function ac3(csp, assignment) 
+    assignment = assignment |> deepcopy
+    queue = csp.checkfunction |> copy
+
+    function domain(var)
+        assignment[var]
+    end
+
+    # removes a value from domain of a variable
+    function remove(var, value)
+        setdiff!(assignment[var], [value])
+    end
+
+    for cs in queue
+        var = cs.uses[1]
+        f = cs.checkfunction
+        for value in domain(var)
+            some = [v2 for v2 in domain(cs.uses[2]) if f(value, v2)]
+            if length(some) == 0
+                # mutation of assignment
+                remove(var, value)
+                list = filter(csp.checkfunction) do cf
+                    if var in cf.uses[2:end]
+                        push!(queue, cf)
+                    end
+                end
+            end
+        end
+    end
+
+    assignment
+end
+
+
+
+function backtrace(csp, assignment :: Dict{Tuple{Int, Int}, Set{Int}})
+    assignment = ac3(csp, assignment)
+    let s = issolution(csp, assignment)
+        if s !== nothing
+            return s
+        end
+    end
+
+    # check csp ?-> return assignment as solution
+    # arc consistency
+
+    # pick next value to be assigned
+    # TODO: MRV or something clever
+    v = filter(assignment |> values) do key
+        size(assignment[key]) > 1
+    end |> first
+
+    for d in csp.domain 
+        assignment[v] = d
+        
+        solution = backtrace(csp, copy(assignment))    
+        if solution !== nothing
+            return solution
+        end
+    end
+
+    return nothing
+end
