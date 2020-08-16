a8cc2ff7f4aab7e1115ba3b8cd3a5692ba04e456
Nils Martel <nils.martel@graphmasters.net>
2020-08-16 12:35:56 +0200

    Rename file, improve tests

--- test.jl
+++ test.jl
@@ -8,15 +8,17 @@ Testing out AC3
 
 let csp = Csp([1, 2, 3], [1, 2], [
                                   Constraint([1, 2], ==)
-                                  Constraint([1, 3], >)
+                                  Constraint([1, 3], <)
                                  ])
     # expected solution
     expected = Dict(1 => 1, 2 => 1, 3 => 2)
 
     solution = backtrace(csp)
 
-    if solution !== expected
-        println("solution should be equal to expected")
+    if solution !== nothing
+        println("(PASSED) solution found!")
         println(solution)
+    elseif
+        println("(FAILED) no solution found!")
     end
 end
